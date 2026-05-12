f984ca8b1df03d50e4bf39ef46d8388c57f84ce4
apteczkaft <k.legionmoskva@gmail.com>
2026-05-12 14:38:18 +0200

    Update ExampleMixin.java

--- gith/src/main/java/com/example/mixin/ExampleMixin.java
+++ gith/src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,43 @@
-package com.example.mixin;
+package com.example;
 
-import net.minecraft.server.MinecraftServer;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.network.chat.Component;
+import com.mojang.blaze3d.platform.InputConstants;
+import org.lwjgl.glfw.GLFW;
 
-@Mixin(MinecraftServer.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+public class ExampleMod implements ClientModInitializer {
+
+    private static KeyMapping keyBinding;
+
+    @Override
+    public void onInitializeClient() {
+        // Регистрация кнопки R (можно поменять GLFW_KEY_R на любую другую)
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "Включить Автобай",
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_R,
+                "FunTime Helper"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            // Проверка нажатия кнопки
+            while (keyBinding.consumeClick()) {
+                AutoBuy.enabled = !AutoBuy.enabled;
+                
+                if (client.player != null) {
+                    String status = AutoBuy.enabled ? "§aВКЛ" : "§cВЫКЛ";
+                    client.player.displayClientMessage(
+                        Component.literal("§6[AutoBuy] §fСтатус: " + status), 
+                        true
+                    );
+                }
+            }
+
+            // Запуск логики
+            AutoBuy.onTick();
+        });
+    }
 }
